651e64516b7fdd4bbc0e2fbb64440ac555a01483
mobarak19 <mobarakhossen2012@gmail.com>
2022-11-30 11:02:56 +0600

    first commit

---
+++ TableStickyHeader/StickyHeader.swift
@@ -0,0 +1,20 @@
+//
+//  StickyHeader.swift
+//  TableStickyHeader
+//
+//  Created by Genusys Inc on 11/30/22.
+//
+
+import UIKit
+
+class StickyHeader: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
