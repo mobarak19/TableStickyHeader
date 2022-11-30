106817f3eb71e44bddc6dade59ab07403d192b71
mobarak19 <mobarakhossen2012@gmail.com>
2022-11-30 11:03:08 +0600

    first commit

--- TableStickyHeader/StickyHeader.swift
+++ TableStickyHeader/StickyHeader.swift
@@ -9,12 +9,56 @@ import UIKit
 
 class StickyHeader: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    public let imageView : UIImageView = {
+        let imgView  = UIImageView()
+        imgView.clipsToBounds = true
+        imgView.contentMode = .scaleAspectFill
+        return imgView
+    }()
+    
+    private var imageViewHeight = NSLayoutConstraint()
+    private var imageViewBottom = NSLayoutConstraint()
+    private var containerView = UIView()
+    private var containerViewHeight = NSLayoutConstraint()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createViews()
+        setViewConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func createViews(){
+        addSubview(containerView)
+        containerView.addSubview(imageView)
+    }
+    private func setViewConstraint(){
+        NSLayoutConstraint.activate([
+            widthAnchor.constraint(equalTo: containerView.widthAnchor),
+            centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            heightAnchor.constraint(equalTo: containerView.heightAnchor)
+        ])
+        
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.widthAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
+        containerViewHeight = containerView.heightAnchor.constraint(equalTo: self.heightAnchor)
+        containerViewHeight.isActive = true
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageViewBottom = imageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        imageViewBottom.isActive = true
+        imageViewHeight = imageView.heightAnchor.constraint(equalTo: containerView.heightAnchor)
+        imageViewHeight.isActive = true
+        
+     }
+    
+    public func scrollViewDidScroll(scrollview:UIScrollView){
+        containerViewHeight.constant = scrollview.contentInset.top
+        let offsetY = -(scrollview.contentOffset.y + scrollview.contentInset.top)
+        containerView.clipsToBounds = offsetY <= 0
+        imageViewBottom.constant = offsetY >= 0 ? 0 : -offsetY/2
+        imageViewHeight.constant = max(offsetY + scrollview.contentInset.top,scrollview.contentInset.top)
+    }
 }
--- TableStickyHeader/ViewController.swift
+++ TableStickyHeader/ViewController.swift
@@ -9,11 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let table : UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    private let countryNames:[String] = [
+        "New York","London","Paris","Kioro","DC","New York","London","Paris","Kioro","DC",
+        "New York","London","Paris","Kioro","DC","New York","London","Paris","Kioro","DC",
+        "New York","London","Paris","Kioro","DC","New York","London","Paris","Kioro","DC"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(table)
+        table.delegate = self
+        table.dataSource = self
+        let header = StickyHeader(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.width))
+        header.imageView.image = UIImage(named: "image")
+        table.tableHeaderView = header
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
     }
 
 
 }
 
+extension ViewController:UITableViewDelegate,UITableViewDataSource,UIScrollViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        countryNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = countryNames[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let header = table.tableHeaderView as? StickyHeader else {return}
+        header.scrollViewDidScroll(scrollview: table)
+    }
+}
